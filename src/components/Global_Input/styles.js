e2c229899dda13ffa92526c398cd61a8ae8b569b
Nando Rodrigo <nandofelicianorodrigo@gmail.com>
2021-08-12 20:23:45 -0300

    fix: apply color on icon

--- src/components/Global_Input/styles.js
+++ src/components/Global_Input/styles.js
@@ -4,7 +4,8 @@ export const StyledInput = styled.div`
   border-bottom: 1px solid var(--medium-gray);
   display: flex;
   align-items: center;
-  margin: 0 2px;
+  margin: 0 10px;
+  color: var(--medium-gray);
 
   input {
     flex: 1;
@@ -14,10 +15,10 @@ export const StyledInput = styled.div`
 
   svg {
     margin-right: 10px;
-    color: var(--medium-gray);
   }
 
   :hover {
     border-bottom: 1px solid var(--light-green);
+    color: var(--light-green);
   }
 `;
