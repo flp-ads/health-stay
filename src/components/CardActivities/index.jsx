b1be4b0a14cd2a9bfdbc601af0619f3c955797ab
Rafael Bertoldo <jagoxitz@gmail.com>
2021-08-12 19:36:42 -0300

    feat: completed activity card

---
+++ src/components/CardActivities/index.jsx
@@ -0,0 +1,35 @@
+import {
+  Background,
+  CardActivitiesContainer,
+  CardActivitiesContent,
+  CardActivitiesInfoContainer,
+  CardActivitiesTitleContainer,
+} from "./style";
+
+import { FiTrash2 } from "react-icons/fi";
+
+export default function CardActivities() {
+  return (
+    <CardActivitiesContainer>
+      <Background />
+      <CardActivitiesContent>
+        <CardActivitiesTitleContainer>
+          <h3>Activitie name</h3>
+          <button>
+            <FiTrash2 size={20} />
+          </button>
+        </CardActivitiesTitleContainer>
+        <CardActivitiesInfoContainer>
+          <div>
+            <h3>Day</h3>
+            <p>00/00/00</p>
+          </div>
+          <div>
+            <h3>Time</h3>
+            <p>00:00</p>
+          </div>
+        </CardActivitiesInfoContainer>
+      </CardActivitiesContent>
+    </CardActivitiesContainer>
+  );
+}
