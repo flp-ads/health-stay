f43afc33da567a300d3e98c1f3d75b4ad8b55525
Eduardo Parraga <eduardo.parraga95@gmail.com>
2021-08-13 15:08:25 -0300

    feat: login mockup still developing

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,8 +1,71 @@
+import Input from '../../components/Global_Input';
+import Button from '../../components/Global_Button';
+
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+import { Link } from "react-router-dom";
+
+import { Container, Background, Content } from './styles';
+
+import { FiLock, FiMail } from "react-icons/fi";
+
 const Login = () => {
+
+    const formSchema = yup.object().shape({
+        email: yup
+            .string()
+            .required('E-mail required')
+            .email('Invalid e-mail'),
+        password: yup
+            .string()
+            .required('Password required'),
+    })
+
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(formSchema)
+    })
+
+    const onFormSubmit = () => {
+
+    }
+
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <Background>
+                <h1>Bem-vindo</h1>
+                <h1 className='title_bold'>de volta</h1>
+            </Background>
+
+            <Content>
+                <form>
+                    <Input
+                        icon={FiMail}
+                        placeholder='Usuário' />
+
+                    <Input
+                        icon={FiLock}
+                        placeholder='Senha' />
+                </form>
+
+                <Button>
+                    login
+                </Button>
+
+                <div className='separator'>
+                    <hr />
+                    <p>ou</p>
+                    <hr />
+                </div>
+
+                <Button
+                    isVanilla>
+                    cadastro
+                </Button>
+
+            </Content>
+        </Container>
     )
 }
 
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -1,2 +1,68 @@
 import styled from "styled-components";
+import SideImage from "../../assets/side.svg";
+import SideMobileImage from "../../assets/mobile_side.svg";
 
+export const Container = styled.div`
+  min-height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media (min-width: 768px) {
+      flex-direction: row;
+
+  }
+`;
+
+export const Background = styled.div`
+  background: url(${SideMobileImage}) no-repeat center, var(--light-green);
+  background-size: 100%;
+  height: 300px;
+  width: 100%;
+
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  justify-content: center;
+
+  h1 {
+    color: var(--dark-green);
+  }
+
+  .title_bold {
+    font-weight: 700;
+  }
+  @media (min-width: 768px) {
+    height: 100vh;
+    width: 35%;
+    background: url(${SideImage}) no-repeat center, var(--light-green);
+    background-size: 100%;
+
+    .title_bold {
+      font-weight: 700;
+    }
+  }
+`;
+
+export const Content = styled.div`
+  width: 50%;
+  margin: 0 auto;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+
+  margin-top: 32px;
+  
+  form {
+    margin-bottom: 24px;
+  }
+
+  button {
+    font-size: 18px;
+  }
+
+  @media (min-width: 768px) {
+  }
+`;
