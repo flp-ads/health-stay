3cbe82a2b577c90ed8082fbb40b3b69ed0779077
BlaringShelf777 <56661141+BlaringShelf777@users.noreply.github.com>
2021-08-13 11:00:36 -0300

    feat: Group Card
    Integrating the team pattern

--- src/components/Group_Card/card.style.js
+++ src/components/Group_Card/card.style.js
@@ -11,55 +11,6 @@ export const Container = styled(motion.div)`
 	overflow: hidden;
 	margin: 1rem;
 	border: 1px solid var(--vanilla);
-
-	div.groupCard__image_container {
-		background-color: var(--light-green);
-		background-image: url(${bg_img});
-		background-size: 175%;
-		background-position: -25px -30px;
-		background-repeat: no-repeat;
-		width: 100px;
-	}
-
-	div.groupCard__info_container {
-		padding: 1rem;
-		position: relative;
-		display: flex;
-		justify-content: space-between;
-		align-items: flex-end;
-		flex-direction: column;
-		flex: 1;
-
-		div.inf__text_container {
-			width: 100%;
-			h4 {
-				font-size: var(--card-title-size);
-				color: var(--dark-green);
-			}
-
-			p {
-				font-size: var(--card-text-size);
-				color: var(--medium-gray);
-			}
-		}
-
-		div.info__icon_container {
-			position: absolute;
-			padding: 1rem;
-			top: 0;
-			right: 0;
-			color: ${(props) =>
-				props.isSubscribed ? css`var(--red)` : css`var(--medium-gray)`};
-		}
-
-		div.info__users {
-			position: relative;
-			width: min-content;
-			/* justify-content: flex-end; */
-			height: 25px;
-			width: 125px;
-		}
-	}
 `
 
 export const UsersBottom = styled(motion.div)`
@@ -77,4 +28,52 @@ export const UsersBottom = styled(motion.div)`
 	height: 25px;
 	border-radius: 50%;
 	box-shadow: 2px 0 2px rgba(0, 0, 0, 0.25);
+`
+
+export const CardImage = styled.div`
+	background-color: var(--light-green);
+	background-image: url(${bg_img});
+	background-size: 175%;
+	background-position: -25px -30px;
+	background-repeat: no-repeat;
+	width: 100px;
+`
+
+export const Text = styled.div`
+	width: 100%;
+
+	h4 {
+		font-size: var(--card-title-size);
+		color: var(--dark-green);
+	}
+
+	p {
+		font-size: var(--card-text-size);
+		color: var(--medium-gray);
+	}
+`
+
+export const Icon = styled.div`
+	position: absolute;
+	padding: 1rem;
+	top: 0;
+	right: 0;
+	color: ${(props) =>
+		props.isSubscribed ? css`var(--red)` : css`var(--medium-gray)`};
+`
+export const Users = styled.div`
+	position: relative;
+	width: min-content;
+	height: 25px;
+	width: 125px;
+`
+
+export const Info = styled.div`
+	padding: 1rem;
+	position: relative;
+	display: flex;
+	justify-content: space-between;
+	align-items: flex-end;
+	flex-direction: column;
+	flex: 1;
 `
--- src/components/Group_Card/index.jsx
+++ src/components/Group_Card/index.jsx
@@ -1,47 +1,60 @@
-import { Container, UsersBottom } from "./card.style"
+import {
+	CardImage,
+	Container,
+	Icon,
+	Info,
+	Text,
+	Users,
+	UsersBottom,
+} from "./card.style"
 import { FaHeart } from "react-icons/fa"
 
 const GroupCard = ({ group, isSubscribed = false }) => {
 	const { name, category, users_on_group } = group
-	let isBig = false
-	let users = users_on_group
-	// const isSubscribed = users_on_group.find(u => u.id == users_on_group.id)
+	// const isSubscribed = users_on_group.find(u => u.id == user.id)
 
-	const handleUsersList = (users, initial = 0) =>
-		users.map(({ username, id }, i) => (
-			<UsersBottom key={id} index={i + initial}>
-				{username[0]}
-			</UsersBottom>
-		))
+	const handleUsers = () => {
+		let isBig = false
+		let users = users_on_group
 
-	if (users_on_group.length > 4) {
-		isBig = true
+		const createList = (users, initial = 0) =>
+			users.map(({ username, id }, i) => (
+				<UsersBottom key={id} index={i + initial}>
+					{username[0]}
+				</UsersBottom>
+			))
 
-		users = users_on_group.slice(0, 4)
+		if (users_on_group.length > 4) {
+			isBig = true
+
+			users = users_on_group.slice(0, 4)
+		}
+
+		return (
+			<>
+				{users.length && createList(users, isBig)}
+				{isBig && (
+					<UsersBottom key="overflow" index={0} Overflow>{`+${
+						users_on_group.length - 4
+					}`}</UsersBottom>
+				)}
+			</>
+		)
 	}
 
-	console.log(users_on_group)
-
 	return (
-		<Container isSubscribed>
-			<div className="groupCard__image_container"></div>
-			<div className="groupCard__info_container">
-				<div className="inf__text_container">
+		<Container>
+			<CardImage />
+			<Info>
+				<Text>
 					<h4>{name}</h4>
 					<p>{category}</p>
-				</div>
-				<div className="info__icon_container">
+				</Text>
+				<Icon isSubscribed>
 					<FaHeart />
-				</div>
-				<div className="info__users">
-					{users.length !== 0 ? handleUsersList(users, isBig) : null}
-					{isBig ? (
-						<UsersBottom key="overflow" index={0} Overflow>{`+${
-							users_on_group.length - 4
-						}`}</UsersBottom>
-					) : null}
-				</div>
-			</div>
+				</Icon>
+				<Users>{handleUsers()}</Users>
+			</Info>
 		</Container>
 	)
 }
