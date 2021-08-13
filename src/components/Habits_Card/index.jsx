adce99136fef968e3159df921583e57d6ad9d312
BlaringShelf777 <56661141+BlaringShelf777@users.noreply.github.com>
2021-08-13 17:29:00 -0300

    fix: Habits Card
    Card had the wrong path to the style file

--- src/components/Habits_Card/index.jsx
+++ src/components/Habits_Card/index.jsx
@@ -1,42 +1,42 @@
 import {
-  Background,
-  HabitsCardContainer,
-  HabitsCardContent,
-  HabitsCardHeaderContainer,
-  HabitsCardInfoContainer,
-  HabitsCardTitleContainer,
-  HabitsCardFooterContainer,
-} from "./Card.style";
+	Background,
+	HabitsCardContainer,
+	HabitsCardContent,
+	HabitsCardHeaderContainer,
+	HabitsCardInfoContainer,
+	HabitsCardTitleContainer,
+	HabitsCardFooterContainer,
+} from "./card.style"
 
-import { FiTrash2, FiCheckCircle } from "react-icons/fi";
+import { FiTrash2, FiCheckCircle } from "react-icons/fi"
 
 const HabitsCard = () => {
-  return (
-    <HabitsCardContainer>
-      <Background />
-      <HabitsCardContent>
-        <HabitsCardHeaderContainer>
-          <strong>Frequency</strong>
-          <p>Difficulty</p>
-        </HabitsCardHeaderContainer>
-        <HabitsCardTitleContainer>
-          <h4>Habit name</h4>
-        </HabitsCardTitleContainer>
-        <HabitsCardInfoContainer>
-          <p>Category</p>
-        </HabitsCardInfoContainer>
-        <HabitsCardFooterContainer>
-          <button>
-            <FiTrash2 size={23} color={"var(--red)"} />
-          </button>
+	return (
+		<HabitsCardContainer>
+			<Background />
+			<HabitsCardContent>
+				<HabitsCardHeaderContainer>
+					<strong>Frequency</strong>
+					<p>Difficulty</p>
+				</HabitsCardHeaderContainer>
+				<HabitsCardTitleContainer>
+					<h4>Habit name</h4>
+				</HabitsCardTitleContainer>
+				<HabitsCardInfoContainer>
+					<p>Category</p>
+				</HabitsCardInfoContainer>
+				<HabitsCardFooterContainer>
+					<button>
+						<FiTrash2 size={23} color={"var(--red)"} />
+					</button>
 
-          <button>
-            <FiCheckCircle size={23} />
-          </button>
-        </HabitsCardFooterContainer>
-      </HabitsCardContent>
-    </HabitsCardContainer>
-  );
-};
+					<button>
+						<FiCheckCircle size={23} />
+					</button>
+				</HabitsCardFooterContainer>
+			</HabitsCardContent>
+		</HabitsCardContainer>
+	)
+}
 
-export default HabitsCard;
+export default HabitsCard
