e4ff47700d5652367f933666951044a84d3c84b4
Eduardo Parraga <eduardo.parraga95@gmail.com>
2021-08-16 11:03:19 -0300

    fix: error message handling

--- src/components/Global_Input/index.jsx
+++ src/components/Global_Input/index.jsx
@@ -1,13 +1,13 @@
 import { ErrorMessage, StyledInput } from "./input.style"
 
-const Input = ({ icon: Icon, register, name, error, ...rest }) => {
+const Input = ({ icon: Icon, register, name, error = '', ...rest }) => {
 	return (
 		<StyledInput isDenied={!!error}>
 			<div>
 				{Icon && <Icon />}
 				<input {...register(name)} {...rest} />
 			</div>
-			<ErrorMessage>{error?.message}</ErrorMessage>
+			<ErrorMessage>{error}</ErrorMessage>
 		</StyledInput>
 	)
 }
