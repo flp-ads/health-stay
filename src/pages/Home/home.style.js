c5464b476512525bbbc13655efae6137e2a8fab8
Felipe Silva <felipesilva.98ads@gmail.com>
2021-08-15 22:52:00 -0300

    feat/ create Home component

---
+++ src/pages/Home/home.style.js
@@ -0,0 +1,175 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: var(--vanilla);
+
+  width: 100%;
+  min-height: calc(100vh - calc(45px + 1rem));
+  margin-top: calc(45px + 1rem);
+  display: flex;
+  flex-direction: column;
+
+  @media (min-width: 768px) {
+    min-height: calc(100vh - calc(45px + 2rem));
+    flex-direction: row;
+  }
+`;
+
+export const HomeTitleContainer = styled.div`
+  background-color: var(--vanilla);
+  height: 60vh;
+  display: flex;
+  justify-content: center;
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: left;
+    margin-top: 15%;
+
+    p {
+      font-size: 1.7rem;
+      color: var(--dark-green);
+    }
+
+    strong {
+      font-size: 2rem;
+      color: var(--light-green);
+    }
+  }
+
+  @media (min-width: 768px) {
+    min-height: calc(100vh - calc(45px + 2rem));
+    width: 50%;
+    height: 105vh;
+
+    div {
+      p {
+        font-size: 2.5rem;
+      }
+      strong {
+        font-size: 3.7rem;
+      }
+    }
+  }
+`;
+
+export const HomeButtonContainer = styled.div`
+  background-color: var(--white);
+  height: 60vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  flex-direction: column;
+
+  div {
+    margin-top: 1%;
+  }
+
+  fieldset {
+    margin-top: 1%;
+    border-top: 1px solid black;
+    border-left: none;
+    border-right: none;
+    border-bottom: none;
+    display: block;
+    text-align: center;
+    height: 45px;
+    width: 270px;
+
+    @media (min-width: 768px) {
+      height: 63px;
+      width: 324px;
+      margin-top: 5%;
+    }
+  }
+
+  fieldset legend {
+    padding: 5px 10px;
+    color: var(--light-green);
+  }
+
+  @media (min-width: 768px) {
+    min-height: calc(100vh - calc(45px + 2rem));
+    width: 50%;
+    height: 105vh;
+  }
+`;
+
+export const Header = styled.header`
+  background-color: var(--light-green);
+  position: fixed;
+  top: 0;
+  left: 0;
+  color: var(--white);
+
+  padding: 0.5rem 1rem;
+  width: 100%;
+
+  div.header_container {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  @media (min-width: 768px) {
+    padding: 2rem 1rem;
+  }
+`;
+
+export const BackgroundHeader = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  overflow: hidden;
+  height: 60vh;
+  width: 18vw;
+
+  img {
+    display: none;
+  }
+
+  @media (min-width: 768px) {
+    img {
+      display: block;
+      position: absolute;
+      top: -195px;
+      right: -375px;
+      transform: rotate(-240deg);
+      width: 710px;
+      height: auto;
+    }
+  }
+`;
+
+export const BackgroundImage = styled.div`
+  position: absolute;
+  width: 40vh;
+  height: 27vh;
+  left: 0;
+  top: -17vh;
+
+  img {
+    position: absolute;
+    width: 59vh;
+    height: 40vh;
+    left: -6vh;
+    top: 52vh;
+    transform: rotate(-12.35deg);
+  }
+
+  @media (min-width: 768px) {
+    width: 50%;
+    height: 17vh;
+    left: 0;
+    top: -35vh;
+
+    img {
+      width: 120%;
+      height: 60vh;
+      left: -6vh;
+      top: 75vh;
+      transform: rotate(-8.35deg);
+    }
+  }
+`;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,54 @@
+import Button from "../../components/Global_Button";
+import {
+  BackgroundHeader,
+  BackgroundImage,
+  Container,
+  Header,
+  HomeButtonContainer,
+  HomeTitleContainer,
+} from "./home.style";
+import bg_image from "../../assets/healthy-food-png.png";
+import { useHistory } from "react-router";
+
 const Home = () => {
-  return <div></div>;
+  const history = useHistory();
+
+  const handleNavigation = (path) => {
+    return history.push(path);
+  };
+  return (
+    <Container>
+      <Header>
+        <h1>Logo</h1>
+      </Header>
+      <BackgroundHeader>
+        <img src={bg_image} alt="" />
+      </BackgroundHeader>
+      <HomeTitleContainer>
+        <div>
+          <p>change yours habits</p>
+          <strong>Today</strong>
+        </div>
+      </HomeTitleContainer>
+
+      <BackgroundImage>
+        <img src={bg_image} alt="" />
+      </BackgroundImage>
+
+      <HomeButtonContainer>
+        <Button onClick={() => handleNavigation("/signup")}>sign up now</Button>
+
+        <div>
+          <fieldset>
+            <legend>or</legend>
+          </fieldset>
+        </div>
+        <Button isVanilla onClick={() => handleNavigation("/login")}>
+          Login rigth away
+        </Button>
+      </HomeButtonContainer>
+    </Container>
+  );
 };
 
 export default Home;
