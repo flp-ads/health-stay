27d5a9a2b850896b4cd8da783859f57b5e95e9c4
BlaringShelf777 <56661141+BlaringShelf777@users.noreply.github.com>
2021-08-17 11:21:47 -0300

    feat: User ID
    Decodes the user id from the access token

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import { AnimatePresence } from 'framer-motion';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
-
-import GlobalStyle from './styles/global';
-import Routes from './routes';
+import GlobalStyle from "./styles/global"
+import Routes from "./routes"
 
 function App() {
-  return (
-    <div>
-      <AnimatePresence>
-        <GlobalStyle />
-        <ToastContainer />
-        <Routes />
-      </AnimatePresence>
-    </div>
-  );
+	return (
+		<div>
+			<GlobalStyle />
+			<ToastContainer />
+			<Routes />
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import Providers from './providers'
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import { BrowserRouter } from "react-router-dom"
+import Providers from "./providers"
+import { AnimatePresence } from "framer-motion"
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Providers>
-        <App />
-      </Providers>
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+	<React.StrictMode>
+		<BrowserRouter>
+			<Providers>
+				<AnimatePresence>
+					<App />
+				</AnimatePresence>
+			</Providers>
+		</BrowserRouter>
+	</React.StrictMode>,
+	document.getElementById("root")
+)
