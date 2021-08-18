1831fa02efa6c6749a30d3fb3022a34809d21ff9
Felipe Silva <felipesilva.98ads@gmail.com>
2021-08-18 11:33:07 -0300

    feat/ create Group route and component files

---
+++ src/components/Dashboard_GoalsCreate/index.jsx
@@ -0,0 +1,55 @@
+import { Container } from "./goals.style";
+import Input from "../Global_Input";
+import Button from "../Global_Button";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { BiPencil } from "react-icons/bi";
+import { FiLayers } from "react-icons/fi";
+
+const DashboardGoalsCreate = () => {
+  const formSchema = yup.object().shape({
+    title: yup.string().required("Preencha este campo"),
+    difficulty: yup.string().required("Selecione uma opção"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(formSchema),
+  });
+
+  const onFormSubmit = () => {};
+
+  const difficultyList = [];
+
+  return (
+    <Container>
+      <form id="create_goal_form" onSubmit={handleSubmit(onFormSubmit)}>
+        <Input
+          name="title"
+          placeholder="Título da meta"
+          icon={BiPencil}
+          register={register}
+          error={errors.title?.message}
+        />
+
+        <Input
+          name="difficulty"
+          placeholder="Dificuldade"
+          icon={FiLayers}
+          register={register}
+          error={errors.difficulty?.message}
+          list={difficultyList}
+        />
+      </form>
+      <Button type="submit" form="create_goal_form">
+        Criar meta
+      </Button>
+    </Container>
+  );
+};
+
+export default DashboardGoalsCreate;
--- src/components/Goals_Card/card.style.js
+++ src/components/Goals_Card/card.style.js
@@ -1,82 +1,78 @@
 import styled from "styled-components";
-import CardImage from '../../assets/card_dark.svg';
+import CardImage from "../../assets/card_dark.svg";
 
 export const Container = styled.div`
-    display: flex;
-    flex-flow: row nowrap;
-    align-items: stretch;
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: stretch;
 
-    width: 280px;
+  max-width: 450px;
+  width: 100%;
+  height: 100px;
+
+  margin: 1rem;
+
+  border: 1px solid var(--vanilla);
+  border-radius: 16px;
+  box-sizing: border-box;
+
+  overflow: hidden;
+
+  @media (min-width: 768px) {
     height: 125px;
-
-    margin: 1rem;
-
-    border: 1px solid var(--vanilla);
-    border-radius: 16px;
-    box-sizing: border-box;
-
-    overflow: hidden;
-
-    @media (min-width: 768px) {
-
-        width: 400px;
-
-    }
-`
+  }
+`;
 
 export const ImageContainer = styled.div`
-    background: url(${CardImage}) no-repeat center, var(--dark-green);
-    width: 100px;
-
-`
+  background: url(${CardImage}) no-repeat center, var(--dark-green);
+  width: 100px;
+`;
 
 export const ContentContainer = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  box-sizing: border-box;
+
+  padding: 8px;
+  width: calc(100% - 100px);
+
+  background-color: var(--vanilla);
+
+  font-size: var(--card-text-size);
+
+  header {
+    width: 100%;
     display: flex;
-    flex-flow: column nowrap;
-    box-sizing: border-box;
-    
-    padding: 8px;
-    width: calc(100% - 100px);
+    flex-flow: row nowrap;
+    justify-content: flex-end;
 
-    background-color: var(--vanilla);
-
-    font-size: var(--card-text-size);
-
-    header {
-        width: 100%;
-        display: flex;
-        flex-flow: row nowrap;
-        justify-content: flex-end;
-
-        button {
-            font-size: 20px;
-            outline: none;
-            background: transparent;
-            border: none;
-            margin-right: 4px;
-        }
-
-        .achieved {
-            color: var(--light-green);
-        }
-
-        .completed {
-            color: var(--light-green);
-        }
-
-        .delete {
-            color: var(--red);
-        }
+    button {
+      font-size: 20px;
+      outline: none;
+      background: transparent;
+      border: none;
+      margin-right: 4px;
     }
 
-    h4 {
-        color: var(--dark-green);
-        font-size: var(--card-title-size);
+    .achieved {
+      color: var(--light-green);
     }
 
-    p {
-        color: var(--light-green);
+    .completed {
+      color: var(--light-green);
     }
 
-`
+    .delete {
+      color: var(--red);
+    }
+  }
 
+  h4 {
+    color: var(--dark-green);
+    font-size: var(--card-title-size);
+  }
+
+  p {
+    color: var(--light-green);
+  }
+`;
