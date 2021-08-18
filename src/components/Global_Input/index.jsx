70c011b264f2f723cb807990c82332f130d58701
BlaringShelf777 <56661141+BlaringShelf777@users.noreply.github.com>
2021-08-18 11:47:16 -0300

    feat: Creating groups
    Now the user can create groups

--- src/components/Global_Input/index.jsx
+++ src/components/Global_Input/index.jsx
@@ -1,24 +1,36 @@
-import { ErrorMessage, StyledInput } from "./input.style";
+import { ErrorMessage, StyledInput } from "./input.style"
 
-const Input = ({ icon: Icon, register, name, error = "", list, selectPlaceholder = '' , ...rest  }) => {
-  return (
-    <StyledInput isDenied={!!error}>
-      <div>
-        {Icon && <Icon />}
-        {list ? (
-          <select {...rest}>
-            <option value="" disabled selected>{selectPlaceholder}</option>
-            {list.map((item, index) => (
-              <option key={index}>{item}</option>
-            ))}
-          </select>
-        ) : (
-          <input {...register(name)} {...rest} />
-        )}
-      </div>
-      <ErrorMessage>{error}</ErrorMessage>
-    </StyledInput>
-  );
-};
+const Input = ({
+	icon: Icon,
+	register,
+	name,
+	error = "",
+	list,
+	selectPlaceholder = "",
+	...rest
+}) => {
+	return (
+		<StyledInput isDenied={!!error}>
+			<div>
+				{Icon && <Icon />}
+				{list ? (
+					<select {...register(name)} {...rest} defaultValue="">
+						<option value="" disabled>
+							{selectPlaceholder}
+						</option>
+						{list.map((item, index) => (
+							<option key={index} value={item}>
+								{item}
+							</option>
+						))}
+					</select>
+				) : (
+					<input {...register(name)} {...rest} />
+				)}
+			</div>
+			<ErrorMessage>{error}</ErrorMessage>
+		</StyledInput>
+	)
+}
 
-export default Input;
+export default Input
