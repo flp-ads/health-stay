115c92eba2b4928f72e084042fc32842c78bfe8a
Felipe Silva <felipesilva.98ads@gmail.com>
2021-08-19 15:51:27 -0300

    fix/ style GoalsCard

--- src/components/Goals_Card/card.style.js
+++ src/components/Goals_Card/card.style.js
@@ -1,89 +1,72 @@
-import styled, { css } from "styled-components"
-import CardImage from "../../assets/card_dark.svg"
-
+import styled from "styled-components";
+import CardImage from "../../assets/card_dark.svg";
 export const Container = styled.div`
-	display: flex;
-	flex-flow: row nowrap;
-	align-items: stretch;
-
-	max-width: 450px;
-	width: 100%;
-	height: 100px;
-
-	margin: 1rem;
-
-	border: 1px solid var(--vanilla);
-	border-radius: 16px;
-	box-sizing: border-box;
-
-	overflow: hidden;
-
-	@media (min-width: 768px) {
-		height: 125px;
-	}
-`
-
+  background-color: var(--light-vanilla);
+  height: 125px;
+  border-radius: 1rem;
+  max-width: 450px;
+  min-width: 20vw;
+  width: 100%;
+  display: flex;
+  overflow: hidden;
+  border: 1px solid var(--vanilla);
+  @media only screen and (min-width: 768px) {
+    height: 150px;
+    flex: 100%;
+  }
+  @media only screen and (min-width: 950px) {
+    height: 150px;
+    flex: calc(33% - 2rem);
+  }
+`;
 export const ImageContainer = styled.div`
-	background: url(${CardImage}) no-repeat center, var(--dark-green);
-	width: 100px;
-`
-
+  background-color: var(--dark-green);
+  background-image: url(${CardImage});
+  background-size: 365%;
+  background-position: -35px -30px;
+  background-repeat: no-repeat;
+  width: 50px;
+  @media only screen and (min-width: 768px) {
+    width: 100px;
+    background-size: 175%;
+    background-position-x: -25px;
+  }
+`;
 export const ContentContainer = styled.div`
-	display: flex;
-	flex-flow: column nowrap;
-	box-sizing: border-box;
-
-	padding: 8px;
-	width: calc(100% - 100px);
-
-	background-color: var(--vanilla);
-
-	font-size: var(--card-text-size);
-
-	header {
-		width: 100%;
-		display: flex;
-		flex-flow: row nowrap;
-		justify-content: flex-end;
-
-		button {
-			font-size: 20px;
-			outline: none;
-			background: transparent;
-			border: none;
-			margin-right: 4px;
-
-			svg {
-				color: var(--medium-gray);
-				transition: 0.3s all ease;
-			}
-		}
-		button.completed {
-			svg {
-				color: ${(props) =>
-					props.achieved ? css`var(--light-green)` : css`var(--medium-gray))`};
-			}
-		}
-
-		button.achieved:hover svg {
-			color: var(--light-green);
-		}
-
-		button.completed:hover svg {
-			color: var(--light-green);
-		}
-
-		button.delete:hover svg {
-			color: var(--red);
-		}
-	}
-
-	h4 {
-		color: var(--dark-green);
-		font-size: var(--card-title-size);
-	}
-
-	p {
-		color: var(--light-green);
-	}
-`
+  display: flex;
+  flex-flow: column nowrap;
+  box-sizing: border-box;
+  padding: 8px;
+  width: calc(100% - 100px);
+  background-color: var(--light-vanilla);
+  font-size: var(--card-text-size);
+  header {
+    width: 100%;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: flex-end;
+    button {
+      font-size: 20px;
+      outline: none;
+      background: transparent;
+      border: none;
+      margin-right: 4px;
+    }
+    .achieved {
+      color: var(--light-green);
+    }
+    .completed {
+      color: var(--light-green);
+    }
+    .delete {
+      color: var(--red);
+    }
+  }
+  h4 {
+    color: var(--dark-green);
+    font-size: var(--card-title-size);
+  }
+  p {
+    color: var(--light-green);
+  }
+`;
